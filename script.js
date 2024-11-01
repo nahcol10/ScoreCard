13c4b9574a17b14a7a924bc99ddd3dfb10db1839
lochanpaudel <lochanpaudel10@gmail.com>
2024-11-01 16:48:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//team1
+let  t1 = 0;
+const increaseByOne1 = () =>{
+    t1+=1;
+    document.querySelector(".t1-score").innerText = t1;
+}
+const increaseByTwo1 = () =>{
+    t1+=2;
+    document.querySelector(".t1-score").innerText = t1;
+}
+const increasebyThree1 = () =>{
+    t1+=3;
+    document.querySelector(".t1-score").innerText = t1;
+}
+// team2
+let  t2 = 0;
+const increaseByOne2= () =>{
+    t2+=1;
+    document.querySelector(".t2-score").innerText= t2;
+}
+const increaseByTwo2 = () =>{
+    t2+=2;
+    document.querySelector(".t2-score").innerText= t2;
+}
+const increasebyThree2= () =>{
+    t2+=3;
+    document.querySelector(".t2-score").innerText= t2;
+}
